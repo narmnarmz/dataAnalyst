a152030a6168332d443bb25587810687ababc617
narmnarmz <narmnarmz@gmail.com>
2015-05-13 15:33:23 +0700

    Edit format file input

--- intervalEstimate.php
+++ intervalEstimate.php
@@ -24,10 +24,35 @@
     		$file= fopen("uploads/data.csv", "r");
     		$data = fgetcsv($file);
     		//show data list
-    		print_r($data);
+            $row = 1;
+            $group = array();
+            if (($handle = fopen("uploads/data.csv", "r")) !== FALSE) {
+                while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                    $num = count($data);
+                    $row++;
+                    for ($c=0; $c < $num; $c++) {
+                        array_push($group, $data[$c]);
+                    }
+                }
+                fclose($handle);
+            }
+            print_r($group);
     		echo "<br>";
             function sd_square($x, $mean) { return pow($x - $mean,2); }
             function sd($array) { return sqrt(array_sum(array_map("sd_square", $array, array_fill(0,count($array), (array_sum($array) / count($array)) ) ) ) / (count($array)-1) );   }
+
+            
+            set_error_handler('exceptions_error_handler');
+
+            //case n more than 200
+            function exceptions_error_handler($severity, $message, $filename, $lineno) {
+              if (error_reporting() == 0) {
+                return;
+              }
+              if (error_reporting() & $severity) {
+                throw new ErrorException($message, 0, $severity, $filename, $lineno);
+              }
+            }
 
             $tcsv = file_get_contents("uploads/t-table.csv");
             $lines = explode(PHP_EOL, $tcsv);
@@ -51,18 +76,23 @@
                     <?php 
                     $rel = array("90%", "95%", "98%", "99%", "99.8%" ,"99.9%");
                     $sig = array(0.1, 0.05, 0.02, 0.01, 0.002 ,0.001);
-                    $n = count($data);
-                    $mean = array_sum($data)/$n;
-                    $sd = sd($data);
-                    $t=$tArray[$n-2][$_POST["sig"]];
+                    $n = count($group);
+                    $mean = array_sum($group)/$n;
+                    $sd = sd($group);
+                    try {
+                        $t=$tArray[$n-2][$_POST["sig"]];
+                    } catch (Exception $e) {
+                        $t=$tArray[200][$_POST["sig"]];
+                    }
+                    // $t=$tArray[$n-2][$_POST["sig"]];
                     echo "<td>".$n."</td>";
-                    echo "<td>".$mean."</td>";
-                    echo "<td>".$sd."</td>";
+                    echo "<td>".number_format($mean,3,'.',',')."</td>";
+                    echo "<td>".number_format($sd,3,'.',',')."</td>";
                     echo "<td>".$rel[$_POST["sig"]]."</td>";
                     echo "<td>".$sig[$_POST["sig"]]."</td>";
                     echo "<td>".$t."</td>";
-                    echo "<td>".($mean-($t*($sd/sqrt($n))))."</td>";
-                    echo "<td>".($mean+($t*($sd/sqrt($n))))."</td>";
+                    echo "<td>".number_format(($mean-($t*($sd/sqrt($n)))),3,'.',',')."</td>";
+                    echo "<td>".number_format(($mean+($t*($sd/sqrt($n)))),3,'.',',')."</td>";
                     echo "<br>";
                      ?>
                 </tr>
